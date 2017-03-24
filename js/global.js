b63f7589adb3697ee04688670d21caa1a7888f94
Jeff <jt@aeimarketing.com>
2017-03-24 00:25:20 -0400

    IOS additions

--- js/global.js
+++ js/global.js
@@ -1,12 +1,35 @@
 
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-    window.open = cordova.InAppBrowser.open;
-    openLink('https://discgolfsearch.com',false);
+
     
     
 }
-
+ var url = "https://discgolfsearch.com?ajax=get_events";
+    $.getJSON(url, {
+      
+        format: "json"
+    }).done(function (data) {
+         var html='';
+           
+       $(data.hits).each(function(idx,hit){
+            html +='<div class="event">';
+            html +='<div class="event_date">'+hit.date_start+'</div>';
+            html +='<div class="event_name">'+hit.name+'</div>';
+            html +='<div class="location">'+hit.city+', '+hit.state+', '+hit.zip+'</div>';
+            html +='<div class="type">'+hit.type+'</div>';
+            html +='<div class="status">'+hit.registration_status+'<br/>'+hit.spots+' spots available</div>';
+            html +='</div>';
+            
+       });
+        $('#events').html(html);
+        
+    });
+function loadMap() {
+    window.open = cordova.InAppBrowser.open;
+    openLink('https://discgolfsearch.com',false);
+ 
+}
 function openLink(link_url, url_bar) {
     var bar = 'location=no,clearsessioncache=yes,clearcache=yes';
     var target = '_self';
