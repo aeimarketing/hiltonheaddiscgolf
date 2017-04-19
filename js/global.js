0b7f2481311508c6421f1da05cd2004b649d3ee5
Jeff <jt@aeimarketing.com>
2017-04-19 09:38:12 -0400

    Splash screen changes

--- js/global.js
+++ js/global.js
@@ -1,52 +1,28 @@
 
+var ref='';
+var ext='';
+var main_url='http://discgolfsearch.com';
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-
-    
-    
-
- var url = "https://discgolfsearch.com?ajax=get_events";
-    $.getJSON(url, {
-      
-        format: "json"
-    }).done(function (data) {
-         var html='';
-           
-       $(data.hits).each(function(idx,hit){
-            html +='<div class="event">';
-            html +='<div class="event_date">'+hit.date_start+'</div>';
-            html +='<div class="event_name">'+hit.name+'</div>';
-            html +='<div class="location">'+hit.city+', '+hit.state+', '+hit.zip+'</div>';
-            html +='<div class="type">'+hit.type+'</div>';
-            html +='<div class="status">'+hit.registration_status+'<br/>'+hit.spots+' spots available</div>';
-            html +='</div>';
-            
-       });
-        $('#events').html(html);
-        
-    });
+    loadMap();
+    window.addEventListener("message", receiveMessage, false);
 }
 function loadMap() {
     window.open = cordova.InAppBrowser.open;
-    openLink('https://discgolfsearch.com',false);
- 
+   
+    ref = window.open(main_url, '_self','location=no');
+    ref.addEventListener('loadstop', function() {
+         $('#wrap').show();
+    });
+    ref.addEventListener('loadstart', function (event) {
+       
+    });
 }
-function openLink(link_url, url_bar) {
-    var bar = 'location=no,clearsessioncache=yes,clearcache=yes';
-    var target = '_self';
-    if (url_bar) {
-        bar = 'location=yes';
-        target = '_system';
-    }
-
-    var ref = cordova.InAppBrowser.open(link_url, target, bar);
-     ref.addEventListener('loadstart', function(event) {
-     if (event.url.match("mobile/close")) {
-         ref.close();
-     }
- }); 
-    
-
+function closeMap(){
+       ref.close();
 }
-
-
+function go_link(url){
+   
+    window.open = cordova.InAppBrowser.open;
+    var ext = window.open(event.url, '_system' );
+}
