f30bd0289f537e2d7ae86b38db2f24c291cee910
Jeff <jt@aeimarketing.com>
2017-07-05 11:27:41 -0400

    android change

--- js/global.js
+++ js/global.js
@@ -1,38 +1,42 @@
 
 var ref = '';
 var ext = '';
-var main_url = 'http://discgolfsearch.com';
-document.addEventListener("deviceready", onDeviceReady, false);
+var main_url = 'https://discgolfsearch.com';
+document.addEventListener("deviceready", loadMap, false);
+//document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
     //window.addEventListener("message", receiveMessage, false);
 
     $.ajax({
-        url: "http://discgolfsearch.com?ajax=get_events&jsoncallback=?",
+        url: "https://discgolfsearch.com/?ajax=get_events",
         crossDomain: true,
-        dataType: "json",
-complete     :   function(data) {
-        console.log(data);
-    },
-        success: function (data) {
+        
+         error: function(xhr, errortype, error) {
+            console.log("AJAX: FAIL: " + errortype + " - " + error);
+            $('h1').text("AJAX FAIL");
+        },
+ }).done(function (hits) {
             var html = '';
-            console.log(data);
-            $.each(data, function (idx,val) {
-                var hit=this;
+            console.log(hits);
+            hits = JSON.parse (hits)
+            $.each(hits, function (idx, hit) {
+
                 html += '<div class="event">';
                 html += '<div class="event_date">' + hit.date_start + '</div>';
                 html += '<div class="event_name">' + hit.name + '</div>';
-                html += '<div class="location">' + hit.city + ', ' + hit.state + ', ' + hit + '</div>';
+                html += '<div class="location">' + hit.city + ', ' + hit.state + ', ' + hit.zip + '</div>';
                 html += '<div class="type">' + hit.type + '</div>';
                 html += '<div class="status">' + hit.registration_status + '<br/>' + hit.spots + ' spots available</div>';
                 html += '</div>';
 
             });
             $('#events').html(html);
-        }
+        
 
     });
 }
+
 function loadMap() {
     window.open = cordova.InAppBrowser.open;
 
