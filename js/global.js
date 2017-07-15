d399fbdb99fc5a398e21235d99f2477ee7ef30cb
Jeff <jt@aeimarketing.com>
2017-07-15 15:21:09 -0400

    Auto complete on enter, bugs and layout issues

--- js/global.js
+++ js/global.js
@@ -1,73 +1,96 @@
 
 var ref = '';
 var ext = '';
-var devi = 'android';
-//var dev='apple';
+var url = '';
+var loop = '';
+var old_url = '';
+var l=0;
+var main_url = 'https://discgolfsearch.com?mob';
 
-var main_url = 'https://discgolfsearch.com';
-//document.addEventListener("deviceready", loadMap, false);
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-   
+
     //window.addEventListener("message", receiveMessage, false);
-    if (devi == 'apple') {
-        $.ajax({
-            url: "https://discgolfsearch.com/?ajax=get_events",
-            crossDomain: true,
-            error: function (xhr, errortype, error) {
-                console.log("AJAX: FAIL: " + errortype + " - " + error);
-                $('h1').text("AJAX FAIL");
-            },
-        }).done(function (hits) {
-            var html = '';
 
-            hits = JSON.parse(hits);
-            $.each(hits, function (idx, hit) {
+    $.ajax({
+        url: "https://discgolfsearch.com/?ajax=get_events",
+        crossDomain: true,
+        error: function (xhr, errortype, error) {
+            console.log("AJAX: FAIL: " + errortype + " - " + error);
+            $('h1').text("AJAX FAIL");
+        }
+    }).done(function (hits) {
+        var html = '';
 
-                html += '<div class="event">';
-                html += '<div class="event_date">' + hit.date_start + '</div>';
-                html += '<div class="event_name">' + hit.name + '</div>';
-                html += '<div class="location">' + hit.city + ', ' + hit.state + ', ' + hit.zip + '</div>';
-                html += '<div class="type">' + hit.type + '</div>';
-                html += '<div class="status">' + hit.registration_status + '<br/>' + hit.spots + ' spots available</div>';
-                html += '</div>';
+        hits = JSON.parse(hits);
+        $.each(hits, function (idx, hit) {
 
-            });
-            $('#events').html(html);
-
+            html += '<div class="event">';
+            html += '<div class="event_date">' + hit.date_start + '</div>';
+            html += '<div class="event_name">' + hit.name + '</div>';
+            html += '<div class="location">' + hit.city + ', ' + hit.state + ', ' + hit.zip + '</div>';
+            html += '<div class="type">' + hit.type + '</div>';
+            html += '<div class="status">' + hit.registration_status + '<br/>' + hit.spots + ' spots available</div>';
+            html += '</div>';
 
         });
-    }
-    if(ref==''){
-        loadMap();
-    }else{
-        navigator.app.exitApp();
-    }
+        $('#events').html(html);
+
+
+    });
 }
 
+
 function loadMap() {
-        if (devi != 'apple') {
-            $('#wrap2').hide();
-        }
+
 
     window.open = cordova.InAppBrowser.open;
 
     ref = window.open(main_url, '_self', 'location=no');
-    ref.addEventListener('loadstop', function () {
-        $('#wrap').show();
-        $('#wrap2').hide();
-        ref='';
-    });
     ref.addEventListener('loadstart', function (event) {
+
+
+    });
+
+    ref.addEventListener("loadstop", function () {
+
+        // Clear out the url in localStorage for subsequent opens.
+        ref.executeScript({code: "localStorage.setItem( 'url', '' );"});
+
+        // Start an interval
+        clearInterval(loop);
+        loop = setInterval(function () {
+            l++;
+            // Execute JavaScript to check for the existence of a url in the
+            // child browser's localStorage.
+            ref.executeScript(
+                    {
+                        code: "localStorage.getItem( 'url' )"
+                    },
+                    function (values) {
+
+                        var url = values[ 0 ];
+                        if(old_url!='' && url==''){
+                            old_url='';
+                        }
+                        ref.executeScript({code: "localStorage.setItem( 'url', '' );"});
+                        // If a url was set, clear the interval and close the InAppBrowser.
+                        if (url != '' && url != old_url) {
+                            
+                            old_url = url;
+                            
+                            //open url in local browser
+                            remo = window.open(url, '_system', 'location=yes');
+
+                        }
+                    }
+            );
+
+        }, 500);
 
     });
 }
 function closeMap() {
     ref.close();
 
-}
-function go_link(url) {
-
-    window.open = cordova.InAppBrowser.open;
-    var ext = window.open(event.url, '_system');
 }
