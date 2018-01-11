c8c9a45cb2278878443d42ae9cd2e5030320ac21
JT <developers@aeimarketing.com>
2018-01-11 00:18:35 -0500

    Icons added

--- js/global.js
+++ js/global.js
@@ -4,69 +4,55 @@ var ext = '';
 var url = '';
 var loop = '';
 var old_url = '';
-var l=0;
-var main_url = 'https://discgolfsearch.com?mob';
-
+var l = 0;
+var main_url = 'https://hiltonheaddiscgolf.com';
+ 
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
-    //window.addEventListener("message", receiveMessage, false);
-
-    $.ajax({
-        url: "https://discgolfsearch.com/?ajax=get_events",
+     $.ajax({
+        url: main_url+"/app_front.php",
         crossDomain: true,
         error: function (xhr, errortype, error) {
             console.log("AJAX: FAIL: " + errortype + " - " + error);
             $('h1').text("AJAX FAIL");
         }
-    }).done(function (hits) {
-        var html = '';
-        var old_date='';
-        hits = JSON.parse(hits);
-        $.each(hits, function (idx, hit) {
-           
-            
-            html += '<div class="event">';
-            html += '<div class="event_name">' + hit.name + '</div>';
-             html += '<div class="event_date">' + hit.date_start + '</div>';
-            html += '<div class="type">' + hit.type + '</div>';
-            html += '<div class="location">' + hit.city + ', ' + hit.state + '</div>';
-
-           // html += '<div class="status">' + hit.registration_status + '<br/>' + hit.spots + ' spots available</div>';
-            html += '</div>';
-
-        });
-        $('#events').html(html);
-        loadMap();
+    }).done(function (html) {
+      
+       
+        var old_date = '';
+        
+       
+        $('#front').html(html);
+     
 
     });
+       
+
+   
 }
 
 
-function loadMap() {
+function loadSite() {
 
 
     window.open = cordova.InAppBrowser.open;
 
     ref = window.open(main_url, '_blank', 'location=no,toolbar=no');
-    ref.addEventListener('loadstart', function (event) {
-        
-        
-    });
+
 
     ref.addEventListener("loadstop", function () {
-       $('.hide_first_load').show();
+        $('.hide_first_load').show();
         $('.hide_after_first_load').hide();
         //send events to browser
-        ref.executeScript({code: "$('#imported_events_list').html('"+$('#events').html()+"');$('#event_cal_box').show();"});
-
+        
         // Clear out the url in localStorage for subsequent opens.
         ref.executeScript({code: "localStorage.setItem( 'url', '' );"});
 
         // Start an interval
         clearInterval(loop);
         loop = setInterval(function () {
-            
+
             // Execute JavaScript to check for the existence of a url in the
             // child browser's localStorage.
             ref.executeScript(
@@ -76,16 +62,16 @@ function loadMap() {
                     function (values) {
 
                         var url = values[ 0 ];
-                        
-                        if(old_url!='' && url==''){
-                            old_url='';
+
+                        if (old_url != '' && url == '') {
+                            old_url = '';
                         }
                         ref.executeScript({code: "localStorage.setItem( 'url', '' );"});
                         // If a url was set, clear the interval and close the InAppBrowser.
                         if (url != '' && url != old_url) {
-                            
+
                             old_url = url;
-                            
+
                             //open url in local browser
                             remo = window.open(url, '_system');
 
